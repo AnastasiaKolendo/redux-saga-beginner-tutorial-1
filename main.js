e0d0de91706d136593a3455e85e639a19e804720
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-09-19 15:22:09 -0700

    Add initial setup

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 
+import createSagaMiddleware from 'redux-saga';
+import {helloSaga} from './sagas';
+
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
-
 const action = type => store.dispatch({type})
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(helloSaga);
+
 
 function render() {
   ReactDOM.render(
